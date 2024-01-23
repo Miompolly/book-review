1263619672e788084908e67eefe1fa5d20fb15c7
Miompolly <57joel39@gmail.com>
2024-01-23 16:21:46 +0200

    local query scope

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
     use HasFactory;
 
-    public function review(){
+    public function reviews()
+    {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%'.$title.'%');
     }
 }
